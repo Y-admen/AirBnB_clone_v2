e34e05c6d7bc9acc894b793859b832786c65e11b
Y-admen <yassminash305@gmail.com>
2024-05-18 10:45:57 +0300

    task0

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""flask starter"""
+from flask import Flask
+
+flask = Flask(__name__)
+
+@flask.route("/", strict_slashes=False)
+def hello_HBNB():
+    """
+    Returns the string "Hello HBNB!" when the /hello_HBNB route is accessed.
+    """
+
+    return "Hello HBNB!"
+
+
+if __name__== '__main__':
+        flask.run(host='0.0.0.0', port=5000)
