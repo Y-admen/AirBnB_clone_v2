4c72ad37381df5794b0270579fd6fd139c4cc40b
Y-admen <yassminash305@gmail.com>
2024-05-18 11:33:38 +0300

    task2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""flask starter"""
+
+from flask import Flask
+
+
+flask = Flask(__name__)
+
+
+@flask.route("/", strict_slashes=False)
+def hello_HBNB():
+    """
+    Returns the string "Hello HBNB!" when the /hello_HBNB route is accessed.
+    """
+
+    return "Hello HBNB!"
+
+
+@flask.route("/hbnb")
+def hbnb():
+    """
+    Renders the "HBNB" string in response to a GET request to the "/hbnb".
+    """
+
+    return "HBNB"
+
+
+@flask.route("/c/<text>")
+def dynamic_rout(text):
+        modified_text = text.replace("_", " ")
+        return f'C {modified_text}'
+
+
+if __name__ == '__main__':
+    flask.run(host='0.0.0.0', port=5000)
